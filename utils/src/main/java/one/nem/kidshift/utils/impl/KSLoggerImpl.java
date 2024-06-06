ee7171186b9b1ffb60a951dea6e5e49b16d4b830
ろむねこ <nem@nem.x0.to>
2024-06-06 17:40:57 +0900

    メソッド生成

--- utils/src/main/java/one/nem/kidshift/utils/impl/KSLoggerImpl.java
+++ utils/src/main/java/one/nem/kidshift/utils/impl/KSLoggerImpl.java
@@ -1,4 +1,46 @@
 package one.nem.kidshift.utils.impl;
 
-public class KSLoggerImpl {
+import one.nem.kidshift.utils.KSLogger;
+
+public class KSLoggerImpl implements KSLogger {
+
+    @Override
+    public KSLogger getChildLogger(String tag) {
+        return null;
+    }
+
+    @Override
+    public KSLogger get(String tag) {
+        return null;
+    }
+
+    @Override
+    public void info(String message) {
+
+    }
+
+    @Override
+    public void warn(String message) {
+
+    }
+
+    @Override
+    public void error(String message) {
+
+    }
+
+    @Override
+    public void debug(String message) {
+
+    }
+
+    @Override
+    public void trace(String message) {
+
+    }
+
+    @Override
+    public void fatal(String message) {
+
+    }
 }
