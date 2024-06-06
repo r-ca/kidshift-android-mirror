a402823faf5f255fc050c2e7751748cac1945fc3
ろむねこ <nem@nem.x0.to>
2024-06-06 17:47:13 +0900

    OutputLog実装

--- utils/src/main/java/one/nem/kidshift/utils/impl/KSLoggerImpl.java
+++ utils/src/main/java/one/nem/kidshift/utils/impl/KSLoggerImpl.java
@@ -1,8 +1,18 @@
 package one.nem.kidshift.utils.impl;
 
+import static one.nem.kidshift.utils.enums.LogLevelEnum.INFO;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+
 import one.nem.kidshift.utils.KSLogger;
+import one.nem.kidshift.utils.enums.LogLevelEnum;
+import one.nem.kidshift.utils.models.LogModel;
 
 public class KSLoggerImpl implements KSLogger {
+
+    private ArrayList<LogModel> logs = new ArrayList<LogModel>();
 
     @Override
     public KSLogger getChildLogger(String tag) {
@@ -42,5 +52,46 @@ public class KSLoggerImpl implements KSLogger {
     @Override
     public void fatal(String message) {
 
+    }
+
+    private void log(LogModel log) {
+        addLog(log);
+        outputLog(log);
+    }
+
+    private void addLog(LogModel log) {
+        logs.add(log);
+    }
+
+    private void outputLog(LogModel log) {
+        // ,区切りで出力
+        String tags = log.getTags().length > 0 ? String.join(",", log.getTags()) : "UNTAGGED";
+
+        LogLevelEnum level = log.getLogLevel();
+        String message = log.getMessage();
+
+        switch (level) {
+            case INFO:
+                Log.i(tags, message);
+                break;
+            case WARN:
+                Log.w(tags, message);
+                break;
+            case ERROR:
+                Log.e(tags, message);
+                break;
+            case DEBUG:
+                Log.d(tags, message);
+                break;
+            case TRACE:
+                Log.v(tags, message);
+                break;
+            case FATAL:
+                Log.wtf(tags, message);
+                break;
+            default:
+                Log.i(tags, message);
+                break;
+        }
     }
 }
