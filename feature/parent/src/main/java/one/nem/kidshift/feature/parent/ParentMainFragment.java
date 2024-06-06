897b1dbd4c8387a0b10bf9b86e75fd016e1347b9
ろむねこ <nem@nem.x0.to>
2024-06-06 15:03:54 +0900

    命名ミス修正

--- feature/parent/src/main/java/one/nem/kidshift/feature/parent/ParentMainFragment.java
+++ feature/parent/src/main/java/one/nem/kidshift/feature/parent/ParentMainFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FeatureParentFragment#newInstance} factory method to
+ * Use the {@link ParentMainFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FeatureParentFragment extends Fragment {
+public class ParentMainFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class FeatureParentFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public FeatureParentFragment() {
+    public ParentMainFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class FeatureParentFragment extends Fragment {
      * @return A new instance of fragment FeatureParentFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static FeatureParentFragment newInstance(String param1, String param2) {
-        FeatureParentFragment fragment = new FeatureParentFragment();
+    public static ParentMainFragment newInstance(String param1, String param2) {
+        ParentMainFragment fragment = new ParentMainFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class FeatureParentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feature_parent, container, false);
+        return inflater.inflate(R.layout.fragment_parent_main, container, false);
     }
 }
