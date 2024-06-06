43be051c0e785114f3086dae17ea91883863f38d
ろむねこ <nem@nem.x0.to>
2024-06-06 16:47:57 +0900

    Interface実装

--- utils/src/main/java/one/nem/kidshift/utils/KidShiftLogger.java
+++ utils/src/main/java/one/nem/kidshift/utils/KidShiftLogger.java
@@ -1,4 +1,11 @@
 package one.nem.kidshift.utils;
 
 public interface KidShiftLogger {
+    void getChildLogger(String tag);
+    void info(String message);
+    void warn(String message);
+    void error(String message);
+    void debug(String message);
+    void trace(String message);
+    void fatal(String message);
 }
