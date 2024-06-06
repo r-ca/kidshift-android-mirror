8078f337df185dba8fa306fa19b1a91db1aaebc0
ろむねこ <nem@nem.x0.to>
2024-06-06 16:25:02 +0900

    Applicationクラスを作成

---
+++ app/src/main/java/one/nem/kidshift/KidShiftApplication.java
@@ -0,0 +1,11 @@
+package one.nem.kidshift;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class KidShiftApplication extends Application {
+
+
+}
