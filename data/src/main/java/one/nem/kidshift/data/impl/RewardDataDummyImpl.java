6ac77d63108ee41361d76ee180d0e8ceb935c5c6
ろむねこ <nem@nem.x0.to>
2024-06-07 11:41:50 +0900

    ダミーにログ追加

--- data/src/main/java/one/nem/kidshift/data/impl/RewardDataDummyImpl.java
+++ data/src/main/java/one/nem/kidshift/data/impl/RewardDataDummyImpl.java
@@ -5,18 +5,27 @@ import com.github.javafaker.Faker;
 import javax.inject.Inject;
 
 import one.nem.kidshift.data.RewardData;
+import one.nem.kidshift.utils.KSLogger;
 
 public class RewardDataDummyImpl implements RewardData {
 
     private Faker faker;
 
     @Inject
+    KSLogger logger;
+
+    @Inject
     public RewardDataDummyImpl() {
         faker = new Faker();
+        logger.setTag("RewardDataDummyImpl");
+
     }
 
     @Override
     public Integer getTotalReward() {
-        return faker.number().numberBetween(0, 100000);
+        logger.info("getTotalReward called");
+        Integer reward = faker.number().numberBetween(0, 10000);
+        logger.info("Returning reward: " + reward);
+        return reward;
     }
 }
