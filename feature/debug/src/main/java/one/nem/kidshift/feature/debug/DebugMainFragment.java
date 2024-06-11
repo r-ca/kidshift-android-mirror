5f05536053dde10b9ad5fa9689d49b460aa3399b
ろむねこ <nem@nem.x0.to>
2024-06-11 11:17:42 +0900

    fix

--- feature/debug/src/main/java/one/nem/kidshift/feature/debug/DebugMainFragment.java
+++ feature/debug/src/main/java/one/nem/kidshift/feature/debug/DebugMainFragment.java
@@ -68,7 +68,6 @@ public class DebugMainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_debug_main, container, false);
         return inflater.inflate(R.layout.fragment_debug_main, container, false);
     }
 
