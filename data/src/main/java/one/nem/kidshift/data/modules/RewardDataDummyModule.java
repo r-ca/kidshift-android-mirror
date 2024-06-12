3de04f811454c3c896354b391a822fe5b617a859
ろむねこ <nem@nem.x0.to>
2024-06-12 15:54:28 +0900

    fix conflict

--- data/src/main/java/one/nem/kidshift/data/modules/RewardDataDummyModule.java
+++ data/src/main/java/one/nem/kidshift/data/modules/RewardDataDummyModule.java
@@ -3,13 +3,12 @@ package one.nem.kidshift.data.modules;
 import dagger.Binds;
 import dagger.Module;
 import dagger.hilt.InstallIn;
-import dagger.hilt.android.components.FragmentComponent;
 import dagger.hilt.android.components.ViewModelComponent;
 import one.nem.kidshift.data.RewardData;
 import one.nem.kidshift.data.impl.RewardDataDummyImpl;
 
 @Module
-@InstallIn(FragmentComponent.class)
+@InstallIn(ViewModelComponent.class)
 abstract public class RewardDataDummyModule {
 
     @Binds
--- data/src/main/java/one/nem/kidshift/data/modules/TaskDataDummyModule.java
+++ data/src/main/java/one/nem/kidshift/data/modules/TaskDataDummyModule.java
@@ -1,15 +1,9 @@
 package one.nem.kidshift.data.modules;
 
 import dagger.Binds;
-import dagger.Module;
-import dagger.hilt.InstallIn;
-import dagger.hilt.android.components.FragmentComponent;
-import dagger.hilt.android.components.ViewModelComponent;
 import one.nem.kidshift.data.TaskData;
 import one.nem.kidshift.data.impl.TaskDataDummyImpl;
 
-@Module
-@InstallIn(FragmentComponent.class)
 abstract public class TaskDataDummyModule {
 
     @Binds
