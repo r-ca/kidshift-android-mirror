fa195c825a8f45d6fd5b4fb61de60baba57f8ad4
ろむねこ <nem@nem.x0.to>
2024-06-13 16:07:22 +0900

    JavaDoc追加

--- data/src/main/java/one/nem/kidshift/data/TaskData.java
+++ data/src/main/java/one/nem/kidshift/data/TaskData.java
@@ -5,5 +5,9 @@ import java.util.List;
 import one.nem.kidshift.model.tasks.TaskItemModel;
 
 public interface TaskData {
+    /**
+     * 存在する全てのタスクを取得する
+     * @return List<TaskItemModel> タスクリスト
+     */
     List<TaskItemModel> getTasks();
 }
