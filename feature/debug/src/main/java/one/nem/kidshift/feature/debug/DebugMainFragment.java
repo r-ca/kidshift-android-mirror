2e24917891b31eafcb075c5255d97004a4f41641
ろむねこ <nem@nem.x0.to>
2024-06-13 16:14:54 +0900

    不要なimportを削除

--- feature/debug/src/main/java/one/nem/kidshift/feature/debug/DebugMainFragment.java
+++ feature/debug/src/main/java/one/nem/kidshift/feature/debug/DebugMainFragment.java
@@ -3,7 +3,6 @@ package one.nem.kidshift.feature.debug;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
