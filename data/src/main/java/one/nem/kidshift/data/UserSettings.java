b6c28d202c501df66f8c4f0d149613c880f58926
ろむねこ <nem@nem.x0.to>
2024-06-14 11:33:44 +0900

    TaskDummyImpl実装

--- data/src/main/java/one/nem/kidshift/data/UserSettings.java
+++ data/src/main/java/one/nem/kidshift/data/UserSettings.java
@@ -1,11 +1,9 @@
 package one.nem.kidshift.data;
 
-import android.graphics.Color;
-
 public interface UserSettings {
 
     interface Task {
-        Color getDefaultIconColor();
+        int getDefaultIconColor();
         String getDefaultIconEmoji();
     }
 }
