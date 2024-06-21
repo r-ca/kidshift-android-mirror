159cf15b9fb364f80999a067d6f8a61d4e452ff7
ろむねこ <nem@nem.x0.to>
2024-06-21 10:43:37 +0900

    インタフェース追加

--- data/src/main/java/one/nem/kidshift/data/UserSettings.java
+++ data/src/main/java/one/nem/kidshift/data/UserSettings.java
@@ -2,6 +2,10 @@ package one.nem.kidshift.data;
 
 public interface UserSettings {
 
+    interface Api {
+        String getApiBaseUrl();
+    }
+
     interface Task {
         int getDefaultIconColor();
         String getDefaultIconEmoji();
