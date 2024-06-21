d8595d2ba1201c6bd86c4d12c626cbd137ea47ce
ろむねこ <nem@nem.x0.to>
2024-06-21 09:06:13 +0900

    ChildModel作成, 暫定実装

---
+++ model/src/main/java/one/nem/kidshift/model/childModel.java
@@ -0,0 +1,49 @@
+package one.nem.kidshift.model;
+
+public class childModel {
+    String internalId;
+    String displayName;
+    String homeGroupId;
+
+    public childModel(String internalId, String displayName, String homeGroupId) {
+        this.internalId = internalId;
+        this.displayName = displayName;
+        this.homeGroupId = homeGroupId;
+    }
+
+    public childModel(String internalId, String displayName) {
+        this.internalId = internalId;
+        this.displayName = displayName;
+    }
+
+    public childModel(String internalId) {
+        this.internalId = internalId;
+    }
+
+    public childModel() {
+    }
+
+    public String getInternalId() {
+        return internalId;
+    }
+
+    public void setInternalId(String internalId) {
+        this.internalId = internalId;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getHomeGroupId() {
+        return homeGroupId;
+    }
+
+    public void setHomeGroupId(String homeGroupId) {
+        this.homeGroupId = homeGroupId;
+    }
+}
