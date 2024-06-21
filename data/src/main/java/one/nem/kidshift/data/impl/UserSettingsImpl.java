86c1b6a552bfb91c036bd73f80cf88f8404881cc
ろむねこ <nem@nem.x0.to>
2024-06-21 12:01:42 +0900

    ApiSetting実装

--- data/src/main/java/one/nem/kidshift/data/impl/UserSettingsImpl.java
+++ data/src/main/java/one/nem/kidshift/data/impl/UserSettingsImpl.java
@@ -1,8 +1,14 @@
 package one.nem.kidshift.data.impl;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Objects;
+
 import javax.inject.Inject;
 
 import one.nem.kidshift.data.UserSettings;
+import one.nem.kidshift.utils.SharedPrefUtils;
 import one.nem.kidshift.utils.factory.SharedPrefUtilsFactory;
 
 public class UserSettingsImpl implements UserSettings {
@@ -25,14 +31,34 @@ public class UserSettingsImpl implements UserSettings {
     }
 
     public class ApiSettingImpl implements UserSettings.ApiSetting {
+
+        SharedPrefUtils sharedPrefUtils;
+
+        String apiBaseUrl;
+
+        ApiSettingImpl() {
+            sharedPrefUtils = sharedPrefUtilsFactory.create("user_settings");
+            ApiSettingImpl apiSetting = sharedPrefUtils.getObject("api_setting", ApiSettingImpl.class);
+            // TODO: リフレクションつかって一括でやる？(プロパティ数があまりにも増えるなら?), 三項演算子やめる?, デフォルト値の設定方法を改善する
+            if (apiSetting != null) {
+                apiBaseUrl = apiSetting.getApiBaseUrl() == null ? "https://kidshift-beta.nem.one/" :
+                        Objects.equals(apiSetting.getApiBaseUrl(), "") ? "https://kidshift-beta.nem.one/" : apiSetting.getApiBaseUrl();
+            }
+        }
+
+        private void save() {
+            sharedPrefUtils.saveObject("api_setting", this);
+        }
+
         @Override
         public String getApiBaseUrl() {
-            return "https://kidshift-beta.nem.one/";
+            return apiBaseUrl;
         }
 
         @Override
         public void setApiBaseUrl(String url) {
-
+            apiBaseUrl = url;
+            save();
         }
     }
 
