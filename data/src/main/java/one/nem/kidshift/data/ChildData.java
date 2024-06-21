55517d59aa47062cfdb0e7ef204892faf3d6bd82
ろむねこ <nem@nem.x0.to>
2024-06-21 09:15:28 +0900

    childData実装

--- data/src/main/java/one/nem/kidshift/data/ChildData.java
+++ data/src/main/java/one/nem/kidshift/data/ChildData.java
@@ -1,4 +1,25 @@
 package one.nem.kidshift.data;
 
+import one.nem.kidshift.model.ChildModel;
+
 public interface ChildData {
+
+    /**
+     * 子ユーザー情報取得
+     * @param childId 子ID
+     * @return ChildModel 子ユーザー情報
+     */
+    ChildModel getChild(String childId);
+
+    /**
+     * 子ユーザー情報更新
+     * @param child 子ユーザー情報
+     */
+    void updateChild(ChildModel child);
+
+    /**
+     * 子ユーザー追加
+     * @param child 子ユーザー情報
+     */
+    void addChild(ChildModel child);
 }
