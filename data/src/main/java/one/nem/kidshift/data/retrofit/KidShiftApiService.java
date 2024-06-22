6edeb07e21027c5d1885d81d0f8b47f49c42a7d8
rca <nem@nem.x0.to>
2024-06-23 03:38:34 +0900

    Serviceに追加

--- data/src/main/java/one/nem/kidshift/data/retrofit/KidShiftApiService.java
+++ data/src/main/java/one/nem/kidshift/data/retrofit/KidShiftApiService.java
@@ -1,14 +1,19 @@
 package one.nem.kidshift.data.retrofit;
 
+import one.nem.kidshift.data.retrofit.model.parent.ParentInfoResponse;
 import one.nem.kidshift.data.retrofit.model.parent.auth.ParentLoginRequest;
 import one.nem.kidshift.data.retrofit.model.parent.auth.ParentLoginResponse;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface KidShiftApiService {
 
     @POST("/parent/auth/login")
     Call<ParentLoginResponse> parentLogin(@Body ParentLoginRequest request);
+
+    @GET("/parent/account")
+    Call<ParentInfoResponse> getParentInfo();
 
 }
