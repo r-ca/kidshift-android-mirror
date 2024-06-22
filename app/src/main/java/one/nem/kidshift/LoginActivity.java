b47bc7b3f891fe8809a21b52c4b18c1ad3c8826d
rca <nem@nem.x0.to>
2024-06-22 16:04:42 +0900

    暫定ログイン処理実装

--- app/src/main/java/one/nem/kidshift/LoginActivity.java
+++ app/src/main/java/one/nem/kidshift/LoginActivity.java
@@ -1,6 +1,7 @@
 package one.nem.kidshift;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +9,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.IOException;
+
 import javax.inject.Inject;
 
 import dagger.hilt.android.AndroidEntryPoint;
+import one.nem.kidshift.data.retrofit.KidShiftApiService;
+import one.nem.kidshift.data.retrofit.model.parent.auth.ParentLoginRequest;
+import one.nem.kidshift.data.retrofit.model.parent.auth.ParentLoginResponse;
 import one.nem.kidshift.utils.KSLogger;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @AndroidEntryPoint
 public class LoginActivity extends AppCompatActivity {
@@ -30,8 +39,37 @@ public class LoginActivity extends AppCompatActivity {
             return insets;
         });
 
+        // Retrofit init
+        KidShiftApiService apiService = new Retrofit.Builder()
+                .baseUrl("https://kidshift-beta.nem.one/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(KidShiftApiService.class);
+
+        EditText emailEditText = findViewById(R.id.emailEditText);
+        EditText passwordEditText = findViewById(R.id.passwordEditText);
+
         findViewById(R.id.loginButton).setOnClickListener(v -> {
-            // ログイン処理
+            try {
+                Response<ParentLoginResponse> response = apiService.parentLogin(
+                        new ParentLoginRequest(
+                                emailEditText.getText().toString(),
+                                passwordEditText.getText().toString()
+                        )).execute();
+
+                if (response.isSuccessful()) {
+                    logger.info("Login Success");
+                    logger.debug("AccessToken: " + response.body().getAccessToken());
+                    // ログイン成功時の処理
+                } else {
+                    logger.error("Login Failed");
+                    logger.debug("Response: " + response.errorBody().string());
+                    // ログイン失敗時の処理
+                }
+            } catch (IOException e) {
+                logger.error("IOException");
+                throw new RuntimeException(e);
+            }
         });
 
         // for Debug
