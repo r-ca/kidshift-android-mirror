a5da6c4592773f38fe39d7d9a0e825a64165854a
rca <nem@nem.x0.to>
2024-06-22 15:49:05 +0900

    ParentLoginRequest実装

---
+++ data/src/main/java/one/nem/kidshift/data/retrofit/model/parent/auth/ParentLoginRequest.java
@@ -0,0 +1,27 @@
+package one.nem.kidshift.data.retrofit.model.parent.auth;
+
+public class ParentLoginRequest {
+    private String email;
+    private String password;
+
+    public ParentLoginRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
