b84afe2710fb943c71b1dc6423aba9b91df28f7b
rca <nem@nem.x0.to>
2024-06-23 02:51:21 +0900

    TaskCacheEntity作成

--- data/src/main/java/one/nem/kidshift/data/room/entity/TaskCacheEntity.java
+++ data/src/main/java/one/nem/kidshift/data/room/entity/TaskCacheEntity.java
@@ -1,4 +1,22 @@
 package one.nem.kidshift.data.room.entity;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "task_cache")
 public class TaskCacheEntity {
+
+    @PrimaryKey
+    @ColumnInfo(name = "internal_id")
+    private int internalId;
+
+    @ColumnInfo(name = "display_name")
+    private String displayName;
+
+    @ColumnInfo(name = "icon_emoji")
+    private String iconEmoji;
+
+    @ColumnInfo(name = "reward")
+    private int reward;
 }
