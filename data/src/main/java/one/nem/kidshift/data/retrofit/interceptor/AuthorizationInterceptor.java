86e7fccab92a578d2c71ebaaba94af7d8e90922d
ろむねこ <nem@nem.x0.to>
2024-06-24 10:28:55 +0900

    Fix

--- data/src/main/java/one/nem/kidshift/data/retrofit/interceptor/AuthorizationInterceptor.java
+++ data/src/main/java/one/nem/kidshift/data/retrofit/interceptor/AuthorizationInterceptor.java
@@ -31,41 +31,14 @@ public class AuthorizationInterceptor implements Interceptor {
     @NonNull
     @Override
     public Response intercept(@NonNull Chain chain) throws IOException {
-        Log.d("AuthorizationInterceptor", "intercept");
+        logger.debug("intercept");
         try {
             if (chain.request().header(HEADER_NAME) == null) {
-                Log.d("AuthorizationInterceptor", "Authorization header is null");
+                logger.debug("Authorization header is null");
                 return chain.proceed(chain.request());
             }
             if (!HEADER_VALUE.equals(chain.request().header(HEADER_NAME))) {
-                Log.d("AuthorizationInterceptor", "Authorization header is not valid");
-                return chain.proceed(chain.request());
-            }
-
-//            Log.d("AuthorizationInterceptor", "Authorization header is valid");
-//            Log.d("AuthorizationInterceptor", "Authorization header: " + chain.request().header(HEADER_NAME));
-
-
-            Log.d("AuthorizationInterceptor", "Fetching token from UserSettings");
-
-            if (userSettings == null) {
-                Log.e("AuthorizationInterceptor", "userSettings is null");
-                return chain.proceed(chain.request());
-            } else {
-                Log.d("AuthorizationInterceptor", "userSettings is not null");
-            }
-
-            UserSettings.AppCommonSetting appCommonSetting = userSettings.getAppCommonSetting();
-            if (appCommonSetting == null) {
-                Log.e("AuthorizationInterceptor", "AppCommonSetting is null");
-                return chain.proceed(chain.request());
-            } else {
-                Log.d("AuthorizationInterceptor", "AppCommonSetting is not null");
-            }
-
-            String token = appCommonSetting.getAccessToken();
-            if (token == null) {
-                Log.e("AuthorizationInterceptor", "Token is null");
+                logger.debug("Authorization header is invalid");
                 return chain.proceed(chain.request());
             }
 
