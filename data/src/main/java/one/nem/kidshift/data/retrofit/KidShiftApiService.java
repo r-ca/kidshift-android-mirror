e0c01dc651e0ba675f310f8ca57d2fc58ed2e939
ろむねこ <nem@nem.x0.to>
2024-06-24 09:30:36 +0900

    Headerにplaceholderを追加

--- data/src/main/java/one/nem/kidshift/data/retrofit/KidShiftApiService.java
+++ data/src/main/java/one/nem/kidshift/data/retrofit/KidShiftApiService.java
@@ -1,5 +1,6 @@
 package one.nem.kidshift.data.retrofit;
 
+import one.nem.kidshift.data.retrofit.interceptor.AuthorizationInterceptor;
 import one.nem.kidshift.data.retrofit.model.parent.ParentInfoResponse;
 import one.nem.kidshift.data.retrofit.model.parent.auth.ParentLoginRequest;
 import one.nem.kidshift.data.retrofit.model.parent.auth.ParentLoginResponse;
@@ -7,6 +8,7 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface KidShiftApiService {
@@ -15,6 +17,7 @@ public interface KidShiftApiService {
     Call<ParentLoginResponse> parentLogin(@Body ParentLoginRequest request);
 
     @GET("/parent/account")
-    Call<ParentInfoResponse> getParentInfo(@Header("Authorization") String token);
+    @Headers(AuthorizationInterceptor.HEADER_PLACEHOLDER)
+    Call<ParentInfoResponse> getParentInfo();
 
 }
