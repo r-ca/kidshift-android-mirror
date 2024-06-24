02b3d39aeb102a4089f449b4f2eba2e28df20524
ろむねこ <nem@nem.x0.to>
2024-06-24 09:30:50 +0900

    ダミーの代替を作成

---
+++ data/src/main/java/one/nem/kidshift/data/impl/ParentDataImpl.java
@@ -0,0 +1,23 @@
+package one.nem.kidshift.data.impl;
+
+import javax.inject.Inject;
+
+import one.nem.kidshift.data.ParentData;
+import one.nem.kidshift.model.ParentModel;
+
+public class ParentDataImpl implements ParentData {
+
+    @Inject
+    public ParentDataImpl() {
+    }
+
+    @Override
+    public ParentModel getParent(String parentId) {
+        return null;
+    }
+
+    @Override
+    public void updateParent(ParentModel parent) {
+
+    }
+}
