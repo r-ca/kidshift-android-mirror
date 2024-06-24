385537de2c89dababd9e24954c2f5244a3965c2b
ろむねこ <nem@nem.x0.to>
2024-06-24 09:55:45 +0900

    WIP

--- data/src/main/java/one/nem/kidshift/data/impl/ParentDataImpl.java
+++ data/src/main/java/one/nem/kidshift/data/impl/ParentDataImpl.java
@@ -1,12 +1,15 @@
 package one.nem.kidshift.data.impl;
 
+import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 
 import javax.inject.Inject;
 
 import one.nem.kidshift.data.ParentData;
 import one.nem.kidshift.data.retrofit.KidShiftApiService;
+import one.nem.kidshift.data.retrofit.model.parent.ParentInfoResponse;
 import one.nem.kidshift.model.ParentModel;
+import retrofit2.Call;
 
 public class ParentDataImpl implements ParentData {
 
@@ -19,7 +22,22 @@ public class ParentDataImpl implements ParentData {
 
     @Override
     public CompletableFuture<ParentModel> getParent() {
-        return null;
+        return CompletableFuture.supplyAsync(() -> {
+            ParentInfoResponse parentInfoResponse;
+            Call<ParentInfoResponse> response = kidshiftApiService.getParentInfo();
+            try {
+                parentInfoResponse = response.execute().body();
+                ParentModel parent = new ParentModel();
+                assert parentInfoResponse != null;
+                parent.setInternalId(parentInfoResponse.getId());
+                parent.setEmail(parentInfoResponse.getEmail());
+                parent.setDisplayName(parentInfoResponse.getDisplayName());
+                // TODO: 他のプロパティも処理する
+                return parent;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     @Override
