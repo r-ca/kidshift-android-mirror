6039044b9a17f5da9799a7f9214952017463f214
ろむねこ <nem@nem.x0.to>
2024-06-26 14:52:44 +0900

    暫定実装

--- data/src/main/java/one/nem/kidshift/data/impl/KSActionsImpl.java
+++ data/src/main/java/one/nem/kidshift/data/impl/KSActionsImpl.java
@@ -8,6 +8,7 @@ import one.nem.kidshift.data.KSActions;
 import one.nem.kidshift.data.UserSettings;
 import one.nem.kidshift.data.retrofit.KidShiftApiService;
 import one.nem.kidshift.data.retrofit.model.parent.ParentInfoResponse;
+import one.nem.kidshift.data.retrofit.model.task.TaskListResponse;
 import one.nem.kidshift.model.ParentModel;
 import one.nem.kidshift.utils.KSLogger;
 import retrofit2.Call;
@@ -28,8 +29,27 @@ public class KSActionsImpl implements KSActions {
     }
 
     @Override
-    public CompletableFuture<Void> syncTasks() {
-
+    public CompletableFuture<TaskListResponse> syncTasks() {
+        return CompletableFuture.supplyAsync(() -> {
+            Call<TaskListResponse> call = kidShiftApiService.getTasks();
+            try {
+                Response<TaskListResponse> response = call.execute();
+                if (!response.isSuccessful()) {
+                    logger.error("Error fetching tasks: " + response.errorBody().string());
+                    throw new RuntimeException("Error fetching tasks: " + response.errorBody().string());
+                }
+                TaskListResponse responseBody = response.body();
+                logger.info("Tasks fetched with status: " + response.code());
+                logger.debug("Tasks: " + responseBody.getList());
+//                // Save to cache
+//                userSettings.getCache().setTasks(responseBody.getList());
+//                logger.info("Tasks saved to cache");
+                return responseBody;
+            } catch (Exception e) {
+                logger.error("Error fetching tasks");
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     @Override
