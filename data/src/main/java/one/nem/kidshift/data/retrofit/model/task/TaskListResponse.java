1d05368da4bbcb17103eedf8bf34632c6f1b8b35
ろむねこ <nem@nem.x0.to>
2024-06-26 14:22:38 +0900

    コンストラクタ追加

--- data/src/main/java/one/nem/kidshift/data/retrofit/model/task/TaskListResponse.java
+++ data/src/main/java/one/nem/kidshift/data/retrofit/model/task/TaskListResponse.java
@@ -6,6 +6,15 @@ import java.util.List;
 public class TaskListResponse {
     private List<TaskBaseItem> list;
 
+    // Full
+    public TaskListResponse(List<TaskBaseItem> list) {
+        this.list = list;
+    }
+
+    // Empty
+    public TaskListResponse() {
+    }
+
     // Getters and setters
     public List<TaskBaseItem> getList() {
         return list;
