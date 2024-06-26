e61aa0ea9399b2eb67e78311ddee3e0753b7f5fc
ろむねこ <nem@nem.x0.to>
2024-06-26 14:42:00 +0900

    コンストラクタ作成

--- data/src/main/java/one/nem/kidshift/data/retrofit/model/child/ChildDetailsResponse.java
+++ data/src/main/java/one/nem/kidshift/data/retrofit/model/child/ChildDetailsResponse.java
@@ -7,6 +7,16 @@ public class ChildDetailsResponse extends ChildBaseItem {
     private Date createdAt;
     private String homeGroupId;
 
+    // Constructor
+    public ChildDetailsResponse(String id, String name, Date createdAt, String homeGroupId) {
+        super(id, name);
+        this.createdAt = createdAt;
+        this.homeGroupId = homeGroupId;
+    }
+
+    public ChildDetailsResponse() {
+    }
+
     // Getters and setters
     public Date getCreatedAt() {
         return createdAt;
