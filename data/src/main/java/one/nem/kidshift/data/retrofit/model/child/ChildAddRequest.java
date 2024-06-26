bc351fa664947e010b9514ccd4d0f9c7ae0f20d1
ろむねこ <nem@nem.x0.to>
2024-06-26 14:40:48 +0900

    ChildAddRequest作成

---
+++ data/src/main/java/one/nem/kidshift/data/retrofit/model/child/ChildAddRequest.java
@@ -0,0 +1,15 @@
+package one.nem.kidshift.data.retrofit.model.child;
+
+// Request to add a child
+public class ChildAddRequest {
+    private String name;
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
