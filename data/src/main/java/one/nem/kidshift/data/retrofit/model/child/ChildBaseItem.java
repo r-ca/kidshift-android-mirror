939dac0abbd7cf732614c03bc38556098089945e
ろむねこ <nem@nem.x0.to>
2024-06-26 14:41:37 +0900

    コンストラクタ作成

--- data/src/main/java/one/nem/kidshift/data/retrofit/model/child/ChildBaseItem.java
+++ data/src/main/java/one/nem/kidshift/data/retrofit/model/child/ChildBaseItem.java
@@ -5,6 +5,15 @@ public class ChildBaseItem {
     private String id;
     private String name;
 
+    // Constructor
+    public ChildBaseItem(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public ChildBaseItem() {
+    }
+
     // Getters and setters
     public String getId() {
         return id;
