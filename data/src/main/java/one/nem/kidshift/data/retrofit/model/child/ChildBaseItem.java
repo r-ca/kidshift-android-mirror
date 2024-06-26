362594017694629a314f06c2c39e3ab991f4afd0
ろむねこ <nem@nem.x0.to>
2024-06-26 14:37:53 +0900

    ChildBaseItem作成

---
+++ data/src/main/java/one/nem/kidshift/data/retrofit/model/child/ChildBaseItem.java
@@ -0,0 +1,24 @@
+package one.nem.kidshift.data.retrofit.model.child;
+
+// Base class for children
+public class ChildBaseItem {
+    private String id;
+    private String name;
+
+    // Getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
