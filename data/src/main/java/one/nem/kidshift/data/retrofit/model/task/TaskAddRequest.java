8958106b6d62d10005c9dc5d2801a69e97c411b9
ろむねこ <nem@nem.x0.to>
2024-06-26 16:28:03 +0900

    不要なコメント削除

--- data/src/main/java/one/nem/kidshift/data/retrofit/model/task/TaskAddRequest.java
+++ data/src/main/java/one/nem/kidshift/data/retrofit/model/task/TaskAddRequest.java
@@ -2,7 +2,6 @@ package one.nem.kidshift.data.retrofit.model.task;
 
 import java.util.List;
 
-// Request to add a task, with optional attached children
 public class TaskAddRequest extends TaskBaseItem {
     private List<String> attachedChildren;
 
