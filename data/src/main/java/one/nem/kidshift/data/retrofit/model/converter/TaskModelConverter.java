d9ab46ac5f2c202f3dc59a7e50fb039cdcf9b3ef
rca <nem@nem.x0.to>
2024-06-29 08:11:46 +0900

    Converter実装

--- data/src/main/java/one/nem/kidshift/data/retrofit/model/converter/TaskModelConverter.java
+++ data/src/main/java/one/nem/kidshift/data/retrofit/model/converter/TaskModelConverter.java
@@ -1,5 +1,9 @@
 package one.nem.kidshift.data.retrofit.model.converter;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import one.nem.kidshift.data.retrofit.model.task.TaskListResponse;
 import one.nem.kidshift.data.retrofit.model.task.TaskResponse;
 import one.nem.kidshift.model.tasks.TaskItemModel;
 
@@ -13,5 +17,19 @@ public class TaskModelConverter {
         model.setBgColor(taskResponse.getBgColor() == null ? "" : taskResponse.getBgColor());
         model.setIconEmoji(taskResponse.getIconEmoji() == null ? "" : taskResponse.getIconEmoji());
         return model;
+    }
+
+    public static TaskResponse taskItemModelToTaskResponse(TaskItemModel taskItemModel) {
+        TaskResponse response = new TaskResponse();
+        response.setId(taskItemModel.getId());
+        response.setName(taskItemModel.getName());
+        response.setReward(taskItemModel.getReward());
+        response.setBgColor(taskItemModel.getBgColor());
+        response.setIconEmoji(taskItemModel.getIconEmoji());
+        return response;
+    }
+
+    public static List<TaskItemModel> taskResponseListToTaskItemModelList(TaskListResponse taskListResponse) {
+        return taskListResponse.getList().stream().map(TaskModelConverter::taskResponseToTaskItemModel).collect(Collectors.toList());
     }
 }
