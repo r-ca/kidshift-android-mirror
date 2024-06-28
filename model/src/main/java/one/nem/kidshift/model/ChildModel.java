c01e8e4a92acc28e6f6a21c81ef151dea59436bc
rca <nem@nem.x0.to>
2024-06-29 04:59:23 +0900

    ChildModelをUserBaseModelから継承

--- model/src/main/java/one/nem/kidshift/model/ChildModel.java
+++ model/src/main/java/one/nem/kidshift/model/ChildModel.java
@@ -1,50 +1,7 @@
 package one.nem.kidshift.model;
 
-// TODO: parent, childを共通クラスから継承させる
-public class ChildModel {
-    String internalId;
-    String displayName;
-    String homeGroupId;
+public class ChildModel extends UserBaseModel {
 
-    public ChildModel(String internalId, String displayName, String homeGroupId) {
-        this.internalId = internalId;
-        this.displayName = displayName;
-        this.homeGroupId = homeGroupId;
-    }
+    // Additional fields
 
-    public ChildModel(String internalId, String displayName) {
-        this.internalId = internalId;
-        this.displayName = displayName;
-    }
-
-    public ChildModel(String internalId) {
-        this.internalId = internalId;
-    }
-
-    public ChildModel() {
-    }
-
-    public String getInternalId() {
-        return internalId;
-    }
-
-    public void setInternalId(String internalId) {
-        this.internalId = internalId;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
-
-    public String getHomeGroupId() {
-        return homeGroupId;
-    }
-
-    public void setHomeGroupId(String homeGroupId) {
-        this.homeGroupId = homeGroupId;
-    }
 }
