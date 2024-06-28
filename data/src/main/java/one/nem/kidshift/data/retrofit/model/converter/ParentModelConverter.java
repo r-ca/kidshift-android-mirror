c60398e96c6b244d52b0c702aa61605141728589
rca <nem@nem.x0.to>
2024-06-29 05:06:08 +0900

    ミス修正

--- data/src/main/java/one/nem/kidshift/data/retrofit/model/converter/ParentModelConverter.java
+++ data/src/main/java/one/nem/kidshift/data/retrofit/model/converter/ParentModelConverter.java
@@ -4,9 +4,6 @@ import one.nem.kidshift.data.retrofit.model.parent.ParentInfoResponse;
 import one.nem.kidshift.model.ParentModel;
 
 public class ParentModelConverter {
-    public static ParentModel parentListResponseToParentModel(ParentInfoResponse parentInfoResponse) {
-        return new ParentModel(parentInfoResponse.getId(), parentInfoResponse.getDisplayName(), parentInfoResponse.getEmail());
-    }
 
     public static ParentModel parentInfoResponseToParentModel(ParentInfoResponse parentInfoResponse) {
         return new ParentModel(parentInfoResponse.getId(), parentInfoResponse.getDisplayName(), parentInfoResponse.getEmail());
@@ -14,10 +11,6 @@ public class ParentModelConverter {
 
     public static ParentInfoResponse parentModelToParentInfoResponse(ParentModel parentModel) {
         return new ParentInfoResponse(parentModel.getId(), parentModel.getName(), parentModel.getEmail());
-    }
-
-    public static ParentModel parentModelToParentModel(ParentModel parentModel) {
-        return new ParentModel(parentModel.getId(), parentModel.getName(), parentModel.getEmail());
     }
 
 }
