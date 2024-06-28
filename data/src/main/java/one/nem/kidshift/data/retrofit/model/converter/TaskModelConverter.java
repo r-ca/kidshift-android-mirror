990ccd83fc34cff1878bd04c3238011b24b8cc99
rca <nem@nem.x0.to>
2024-06-29 08:09:00 +0900

    Converter実装

--- data/src/main/java/one/nem/kidshift/data/retrofit/model/converter/TaskModelConverter.java
+++ data/src/main/java/one/nem/kidshift/data/retrofit/model/converter/TaskModelConverter.java
@@ -1,4 +1,17 @@
 package one.nem.kidshift.data.retrofit.model.converter;
 
+import one.nem.kidshift.data.retrofit.model.task.TaskResponse;
+import one.nem.kidshift.model.tasks.TaskItemModel;
+
 public class TaskModelConverter {
+
+    public static TaskItemModel taskResponseToTaskItemModel(TaskResponse taskResponse) {
+        TaskItemModel model = new TaskItemModel();
+        model.setId(taskResponse.getId());
+        model.setName(taskResponse.getName());
+        model.setReward(taskResponse.getReward());
+        model.setBgColor(taskResponse.getBgColor() == null ? "" : taskResponse.getBgColor());
+        model.setIconEmoji(taskResponse.getIconEmoji() == null ? "" : taskResponse.getIconEmoji());
+        return model;
+    }
 }
