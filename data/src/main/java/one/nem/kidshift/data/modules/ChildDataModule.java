5f27ef580c19fd39c77df0ee8ff11c01fca356cb
ろむねこ <nem@nem.x0.to>
2024-06-28 12:38:31 +0900

    モジュール実装

--- data/src/main/java/one/nem/kidshift/data/modules/ChildDataModule.java
+++ data/src/main/java/one/nem/kidshift/data/modules/ChildDataModule.java
@@ -1,4 +1,16 @@
 package one.nem.kidshift.data.modules;
 
-public class ChildDataModule {
+import dagger.Binds;
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import dagger.hilt.components.SingletonComponent;
+import one.nem.kidshift.data.ChildData;
+import one.nem.kidshift.data.impl.ChildDataImpl;
+
+@Module
+@InstallIn(SingletonComponent.class)
+public abstract class ChildDataModule {
+
+    @Binds
+    abstract ChildData provideChildData(ChildDataImpl childDataImpl);
 }
