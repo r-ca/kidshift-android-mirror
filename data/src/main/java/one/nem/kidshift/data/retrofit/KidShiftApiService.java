a96890b3b8b4f7e9f90414769b7f02b850c06c73
ろむねこ <nem@nem.x0.to>
2024-06-28 12:18:00 +0900

    JavaDoc追加, 型修正

--- data/src/main/java/one/nem/kidshift/data/retrofit/KidShiftApiService.java
+++ data/src/main/java/one/nem/kidshift/data/retrofit/KidShiftApiService.java
@@ -6,6 +6,7 @@ import one.nem.kidshift.data.retrofit.model.parent.auth.ParentLoginRequest;
 import one.nem.kidshift.data.retrofit.model.parent.auth.ParentLoginResponse;
 import one.nem.kidshift.data.retrofit.model.task.TaskAddRequest;
 import one.nem.kidshift.data.retrofit.model.task.TaskListResponse;
+import one.nem.kidshift.data.retrofit.model.task.TaskResponse;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
@@ -18,35 +19,79 @@ import retrofit2.http.Path;
 
 public interface KidShiftApiService {
 
+    // Parent APIs
+    // Auth
+
+    /**
+     * 保護者ログイン処理
+     * @param request ParentLoginRequest
+     * @return ParentLoginResponse
+     */
     @POST("/parent/auth/login")
     Call<ParentLoginResponse> parentLogin(@Body ParentLoginRequest request);
 
+    /**
+     * 保護者アカウント情報取得処理
+     * @return ParentInfoResponse
+     */
     @GET("/parent/account")
     @Headers(AuthorizationInterceptor.HEADER_PLACEHOLDER)
     Call<ParentInfoResponse> getParentInfo();
 
+    // Task APIs
+
+    /**
+     * タスク一覧取得
+     * @return TaskListResponse
+     */
     @GET("/parent/task")
     @Headers(AuthorizationInterceptor.HEADER_PLACEHOLDER)
     Call<TaskListResponse> getTasks();
 
+    /**
+     * タスク追加
+     * @param request TaskAddRequest
+     * @return TaskListResponse
+     */
     @POST("/parent/task")
     @Headers(AuthorizationInterceptor.HEADER_PLACEHOLDER)
-    Call<TaskListResponse> addTask(@Body TaskAddRequest request);
+    Call<TaskResponse> addTask(@Body TaskAddRequest request);
 
+    /**
+     * タスク更新
+     * @param request TaskAddRequest
+     * @param id タスクID
+     * @return TaskListResponse
+     */
     @PUT("/parent/task/{id}")
     @Headers(AuthorizationInterceptor.HEADER_PLACEHOLDER)
-    Call<TaskListResponse> updateTask(@Body TaskAddRequest request, @Path("id") String id);
+    Call<TaskResponse> updateTask(@Body TaskAddRequest request, @Path("id") String id);
 
+    /**
+     * タスク削除
+     * @param id タスクID
+     * @return TaskListResponse
+     */
     @DELETE("/parent/task/{id}")
     @Headers(AuthorizationInterceptor.HEADER_PLACEHOLDER)
-    Call<TaskListResponse> removeTask(@Path("id") String id);
+    Call<Void> removeTask(@Path("id") String id); // TODO-rca: OK responseをパース
 
+    /**
+     * タスク詳細取得
+     * @param id タスクID
+     * @return TaskListResponse
+     */
     @GET("/parent/task/{id}")
     @Headers(AuthorizationInterceptor.HEADER_PLACEHOLDER)
-    Call<TaskListResponse> getTask(@Path("id") String id);
+    Call<TaskResponse> getTask(@Path("id") String id);
 
+    /**
+     * タスク完了処理
+     * @param id タスクID
+     * @return
+     */
     @POST("/parent/task/{id}/complete")
     @Headers(AuthorizationInterceptor.HEADER_PLACEHOLDER)
-    Call<TaskListResponse> completeTask(@Path("id") String id);
+    Call<Void> completeTask(@Path("id") String id); // TODO-rca: OK responseをパース
 
 }
