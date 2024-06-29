8b3cfa8d34344b351e93d2ba1a9d8004d2af0cf9
rca <nem@nem.x0.to>
2024-06-29 09:33:06 +0900

    Dao追加

--- data/src/main/java/one/nem/kidshift/data/room/dao/ChildCacheDao.java
+++ data/src/main/java/one/nem/kidshift/data/room/dao/ChildCacheDao.java
@@ -1,7 +1,24 @@
 package one.nem.kidshift.data.room.dao;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+import one.nem.kidshift.data.room.entity.ChildCacheEntity;
+i
 
 @Dao
 public interface ChildCacheDao {
+
+    @Insert
+    void insertChild(ChildCacheEntity child);
+
+    @Query("SELECT * FROM child_cache WHERE id = :childId")
+    ChildCacheEntity getChild(String childId);
+
+    @Query("SELECT * FROM child_cache")
+    List<ChildCacheEntity> getChildList();
+
 }
