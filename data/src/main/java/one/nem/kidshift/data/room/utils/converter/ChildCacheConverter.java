80212f747e11bbd04008a07195978fab41a82bde
rca <nem@nem.x0.to>
2024-06-29 10:24:18 +0900

    Converter追加

--- data/src/main/java/one/nem/kidshift/data/room/utils/converter/ChildCacheConverter.java
+++ data/src/main/java/one/nem/kidshift/data/room/utils/converter/ChildCacheConverter.java
@@ -1,5 +1,9 @@
 package one.nem.kidshift.data.room.utils.converter;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import one.nem.kidshift.data.room.entity.ChildCacheEntity;
 import one.nem.kidshift.model.ChildModel;
 
@@ -10,6 +14,10 @@ public class ChildCacheConverter {
         entity.id = childModel.getId();
         entity.name = childModel.getName();
         return entity;
+    }
+
+    public static List<ChildCacheEntity> childModelToChildCacheEntity(List<ChildModel> childList) {
+        return childList.stream().map(ChildCacheConverter::childModelToChildCacheEntity).collect(Collectors.toList());
     }
 
     public static ChildModel childCacheEntityToChildModel(ChildCacheEntity entity) {
