fc07f605153fa27056713059d57bc859e86afe4d
rca <nem@nem.x0.to>
2024-06-29 09:41:36 +0900

    実装

--- data/src/main/java/one/nem/kidshift/data/room/dao/ChildCacheDao.java
+++ data/src/main/java/one/nem/kidshift/data/room/dao/ChildCacheDao.java
@@ -2,7 +2,9 @@ package one.nem.kidshift.data.room.dao;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
@@ -12,8 +14,11 @@ i
 @Dao
 public interface ChildCacheDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertChild(ChildCacheEntity child);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertChildList(List<ChildCacheEntity> childList);
 
     @Query("SELECT * FROM child_cache WHERE id = :childId")
     ChildCacheEntity getChild(String childId);
