4eb2bd32fdb11d8c51a37f3734edbfc691c29e3d
rca <nem@nem.x0.to>
2024-06-29 11:44:32 +0900

    型修正

--- model/src/main/java/one/nem/kidshift/model/callback/TaskItemModelCallback.java
+++ model/src/main/java/one/nem/kidshift/model/callback/TaskItemModelCallback.java
@@ -1,12 +1,14 @@
 package one.nem.kidshift.model.callback;
 
+import java.util.List;
+
 import one.nem.kidshift.model.tasks.TaskItemModel;
 
 public interface TaskItemModelCallback {
 
     void onUnchanged();
 
-    void onUpdated(TaskItemModel taskItem);
+    void onUpdated(List<TaskItemModel> taskItem);
 
     void onFailed(String message);
 }
