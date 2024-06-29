2a21abfd3c2ae6ac70738de799f48509e6a77e55
rca <nem@nem.x0.to>
2024-06-29 10:30:32 +0900

    TaskCacheConverter実装

---
+++ data/src/main/java/one/nem/kidshift/data/room/utils/converter/TaskCacheConverter.java
@@ -0,0 +1,33 @@
+package one.nem.kidshift.data.room.utils.converter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import one.nem.kidshift.data.room.entity.TaskCacheEntity;
+import one.nem.kidshift.model.tasks.TaskItemModel;
+
+public class TaskCacheConverter {
+
+    public static TaskCacheEntity taskModelToTaskCacheEntity(TaskItemModel taskModel) {
+        TaskCacheEntity entity = new TaskCacheEntity();
+        entity.id = taskModel.getId();
+        entity.name = taskModel.getName();
+        entity.iconEmoji = taskModel.getIconEmoji();
+        entity.reward = taskModel.getReward();
+        return entity;
+    }
+
+    public static List<TaskCacheEntity> taskModelListToTaskCacheEntityList(List<TaskItemModel> taskList) {
+        return taskList.stream().map(TaskCacheConverter::taskModelToTaskCacheEntity).collect(Collectors.toList());
+    }
+
+    public static TaskItemModel taskCacheEntityToTaskModel(TaskCacheEntity entity) {
+        return new TaskItemModel(entity.id, entity.name, entity.iconEmoji, entity.reward);
+    }
+
+    public static List<TaskItemModel> taskCacheEntityListToTaskModelList(List<TaskCacheEntity> entityList) {
+        return entityList.stream().map(TaskCacheConverter::taskCacheEntityToTaskModel).collect(Collectors.toList());
+    }
+
+
+}
