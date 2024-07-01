eab70e801e3b2482c4b6f3f67c29b781e7609731
ろむねこ <nem@nem.x0.to>
2024-07-01 12:18:15 +0900

    JavaDoc

--- data/src/main/java/one/nem/kidshift/data/retrofit/model/converter/ParentModelConverter.java
+++ data/src/main/java/one/nem/kidshift/data/retrofit/model/converter/ParentModelConverter.java
@@ -5,10 +5,20 @@ import one.nem.kidshift.model.ParentModel;
 
 public class ParentModelConverter {
 
+    /**
+     * ParentInfoResponseをParentModelに変換する
+     * @param parentInfoResponse ParentInfoResponse
+     * @return ParentModel
+     */
     public static ParentModel parentInfoResponseToParentModel(ParentInfoResponse parentInfoResponse) {
         return new ParentModel(parentInfoResponse.getId(), parentInfoResponse.getDisplayName(), parentInfoResponse.getEmail());
     }
 
+    /**
+     * ParentModelをParentInfoResponseに変換する
+     * @param parentModel ParentModel
+     * @return ParentInfoResponse
+     */
     public static ParentInfoResponse parentModelToParentInfoResponse(ParentModel parentModel) {
         return new ParentInfoResponse(parentModel.getId(), parentModel.getName(), parentModel.getEmail());
     }
