eefcac51d06e28644e2ab692fb4a40fef2373512
ろむねこ <nem@nem.x0.to>
2024-07-01 12:11:14 +0900

    コンストラクタにJavaDoc追加

--- data/src/main/java/one/nem/kidshift/data/retrofit/model/task/TaskResponse.java
+++ data/src/main/java/one/nem/kidshift/data/retrofit/model/task/TaskResponse.java
@@ -7,22 +7,46 @@ public class TaskResponse extends TaskBaseItem {
 
     // コンストラクタ
     // 全プロパティ
+    /**
+     * コンストラクタ (全プロパティ)
+     * @param id タスクID
+     * @param name タスク名
+     * @param iconEmoji アイコン絵文字
+     * @param bgColor 背景色
+     * @param reward 報酬
+     * @param attachedChildren アタッチされた子供IDリスト
+     */
     public TaskResponse(String id, String name, String iconEmoji, String bgColor, int reward, List<String> attachedChildren) {
         super(id, name, iconEmoji, bgColor, reward);
         this.attachedChildren = attachedChildren;
     }
 
     // 必須プロパティ
+    /**
+     * コンストラクタ (必須プロパティ)
+     * @param id タスクID
+     * @param name タスク名
+     * @param reward 報酬
+     * @param attachedChildren アタッチされた子供IDリスト
+     */
     public TaskResponse(String id, String name, int reward, List<String> attachedChildren) {
         super(id, name, reward);
         this.attachedChildren = attachedChildren;
     }
 
     // 空
+    /**
+     * コンストラクタ (空)
+     */
     public TaskResponse() {
     }
 
     // baseItemを指定して拡張
+    /**
+     * コンストラクタ (baseItemを指定して拡張)
+     * @param taskBaseItem タスクベースアイテム
+     * @param attachedChildren アタッチされた子供IDリスト
+     */
     public TaskResponse(TaskBaseItem taskBaseItem, List<String> attachedChildren) {
         super(taskBaseItem.getId(), taskBaseItem.getName(), taskBaseItem.getIconEmoji(), taskBaseItem.getBgColor(), taskBaseItem.getReward());
         this.attachedChildren = attachedChildren;
