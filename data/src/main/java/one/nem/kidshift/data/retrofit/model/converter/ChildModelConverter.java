ed85755968555f3eee1342ac5e79fa64c3551034
ろむねこ <nem@nem.x0.to>
2024-07-01 12:19:46 +0900

    JavaDoc

--- data/src/main/java/one/nem/kidshift/data/retrofit/model/converter/ChildModelConverter.java
+++ data/src/main/java/one/nem/kidshift/data/retrofit/model/converter/ChildModelConverter.java
@@ -11,16 +11,33 @@ import one.nem.kidshift.model.ChildModel;
 
 public class ChildModelConverter {
 
+    // 日本語
+
+    /**
+     * 子供モデルを子供追加リクエストに変換する
+     * @param childModel 子供モデル
+     * @return 子供追加リクエスト
+     */
     public static ChildAddRequest childModelToChildAddRequest(ChildModel childModel) {
         ChildAddRequest request = new ChildAddRequest();
         request.setName(childModel.getName());
         return request;
     }
 
+    /**
+     * 子供リストレスポンスを子供モデルリストに変換する
+     * @param childListResponse 子供リストレスポンス
+     * @return 子供モデルリスト
+     */
     public static List<ChildModel> childListResponseToChildModelList(ChildListResponse childListResponse) {
         return childListResponse.getList().stream().map(ChildModelConverter::childResponseToChildModel).collect(Collectors.toList());
     }
 
+    /**
+     * ChildResponseをChildModelに変換する
+     * @param childResponse ChildResponse
+     * @return ChildModel
+     */
     public static ChildModel childResponseToChildModel(ChildResponse childResponse) {
         return new ChildModel(childResponse.getId(), childResponse.getName());
     }
