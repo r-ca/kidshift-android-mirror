07ad7ae6728f869b4cf7e76b4b1a3f22571f3965
rca <nem@nem.x0.to>
2024-07-03 23:38:36 +0900

    変換

--- data/src/main/java/one/nem/kidshift/data/impl/ChildDataImpl.java
+++ data/src/main/java/one/nem/kidshift/data/impl/ChildDataImpl.java
@@ -46,33 +46,31 @@ public class ChildDataImpl implements ChildData {
         return CompletableFuture.supplyAsync(() -> {
             logger.debug("子供リスト取得開始");
             AtomicReference<List<ChildModel>> childListTmp = new AtomicReference<>();
-            Thread thread = new Thread(() -> {
-                ksActions.syncChildList().thenAccept(childList -> {
-                    if (childListTmp.get() == null || childListTmp.get().isEmpty()) {
-                        logger.debug("子供リスト取得完了: キャッシュよりはやく取得完了 or キャッシュ無し");
-                        if (childList == null || childList.isEmpty()) {
-                            callback.onUnchanged();
-                        } else {
-                            callback.onUpdated(childList);
-                        }
+            Thread thread = new Thread(() -> ksActions.syncChildList().thenAccept(childList -> {
+                if (childListTmp.get() == null || childListTmp.get().isEmpty()) {
+                    logger.debug("子供リスト取得完了: キャッシュよりはやく取得完了 or キャッシュ無し");
+                    if (childList == null || childList.isEmpty()) {
+                        callback.onUnchanged();
                     } else {
-                        boolean isChanged =
-                                childList.size() != childListTmp.get().size() ||
-                                        childList.stream().anyMatch(child -> childListTmp.get().stream().noneMatch(childTmp -> child.getId().equals(childTmp.getId())));
-                        if (isChanged) {
-                            logger.debug("子供リスト取得完了: キャッシュと比較して変更あり");
-                            callback.onUpdated(childList);
-                        } else {
-                            logger.debug("子供リスト取得完了: キャッシュと比較して変更なし");
-                            callback.onUnchanged();
-                        }
+                        callback.onUpdated(childList);
                     }
-                }).exceptionally(e -> {
-                    logger.error("子供リスト取得失敗: " + e.getMessage());
-                    callback.onFailed(e.getMessage());
-                    return null;
-                });
-            });
+                } else {
+                    boolean isChanged =
+                            childList.size() != childListTmp.get().size() ||
+                                    childList.stream().anyMatch(child -> childListTmp.get().stream().noneMatch(childTmp -> child.getId().equals(childTmp.getId())));
+                    if (isChanged) {
+                        logger.debug("子供リスト取得完了: キャッシュと比較して変更あり");
+                        callback.onUpdated(childList);
+                    } else {
+                        logger.debug("子供リスト取得完了: キャッシュと比較して変更なし");
+                        callback.onUnchanged();
+                    }
+                }
+            }).exceptionally(e -> {
+                logger.error("子供リスト取得失敗: " + e.getMessage());
+                callback.onFailed(e.getMessage());
+                return null;
+            }));
             thread.start();
             return cacheWrapper.getChildList().thenApply(childList -> {
                 if (childList == null || childList.isEmpty()) {
