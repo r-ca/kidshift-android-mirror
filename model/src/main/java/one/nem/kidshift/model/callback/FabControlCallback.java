325ca7b96a4a168f9a268f71eddd8ef5fa1f85ed
ろむねこ <nem@nem.x0.to>
2024-07-05 10:29:03 +0900

    コールバック作成

---
+++ model/src/main/java/one/nem/kidshift/model/callback/FabControlCallback.java
@@ -0,0 +1,4 @@
+package one.nem.kidshift.model.callback;
+
+public interface FabControlCallback {
+}
---
+++ model/src/main/java/one/nem/kidshift/model/callback/FabEventCallback.java
@@ -0,0 +1,7 @@
+package one.nem.kidshift.model.callback;
+
+public interface FabEventCallback {
+    void showFab();
+    void hideFab();
+    void setFabEventCallback(FabEventCallback callback);
+}
