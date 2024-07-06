d27f04dade99fde20be51dce16f0afbc181335da
rca <nem@nem.x0.to>
2024-07-06 12:04:07 +0900

    import整理

--- feature/setting/src/main/java/one/nem/kidshift/feature/setting/SettingMainFragment.java
+++ feature/setting/src/main/java/one/nem/kidshift/feature/setting/SettingMainFragment.java
@@ -12,7 +12,6 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
