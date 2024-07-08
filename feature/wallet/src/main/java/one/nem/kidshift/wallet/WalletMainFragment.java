1db94bc3b6ba2bbeee3215d39563ddb9da0d8637
ろむねこ <nem@nem.x0.to>
2024-07-08 11:29:20 +0900

    WIP

--- feature/wallet/src/main/java/one/nem/kidshift/wallet/WalletMainFragment.java
+++ feature/wallet/src/main/java/one/nem/kidshift/wallet/WalletMainFragment.java
@@ -36,6 +36,10 @@ public class WalletMainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wallet_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_wallet_main, container, false);
+
+
+
+        return view;
     }
 }
