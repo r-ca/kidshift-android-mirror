cfcf11e5574f290ca4a505d1be73d99e49184f61
ろむねこ <nem@nem.x0.to>
2024-07-08 14:05:35 +0900

    アクティビティ作成

---
+++ feature/child/src/main/java/one/nem/kidshift/feature/child/ChildManageMainActivity.java
@@ -0,0 +1,24 @@
+package one.nem.kidshift.feature.child;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ChildManageMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_child_manage_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
