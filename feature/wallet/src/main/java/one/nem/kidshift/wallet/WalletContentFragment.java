d2c5e7e8b32bfb124310f3a72269d353479bcd68
ろむねこ <nem@nem.x0.to>
2024-07-08 12:07:18 +0900

    引数を受け取るように

--- feature/wallet/src/main/java/one/nem/kidshift/wallet/WalletContentFragment.java
+++ feature/wallet/src/main/java/one/nem/kidshift/wallet/WalletContentFragment.java
@@ -1,15 +1,11 @@
 package one.nem.kidshift.wallet;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import javax.inject.Inject;
-
 import dagger.hilt.android.AndroidEntryPoint;
 import one.nem.kidshift.utils.KSLogger;
 import one.nem.kidshift.utils.factory.KSLoggerFactory;
@@ -17,23 +13,34 @@ import one.nem.kidshift.utils.factory.KSLoggerFactory;
 @AndroidEntryPoint
 public class WalletContentFragment extends Fragment {
 
+    private static final String ARG_CHILD_ID = "childId";
+
     @Inject
     KSLoggerFactory loggerFactory;
 
     private KSLogger logger;
+    private String childId;
 
     public WalletContentFragment() {
         // Required empty public constructor
     }
 
-    public static Fragment newInstance() { // WIP
-        return new WalletContentFragment();
+    public static WalletContentFragment newInstance(String childId) {
+        WalletContentFragment fragment = new WalletContentFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_CHILD_ID, childId);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            childId = getArguments().getString(ARG_CHILD_ID);
+        }
         logger = loggerFactory.create("WalletMainFragment");
+        logger.debug("Received parameter: " + childId);
     }
 
     @Override
@@ -41,9 +48,7 @@ public class WalletContentFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_wallet_content, container, false);
-
-
-
+        // Use the parameter as needed in the view setup
         return view;
     }
 }
