94efb7ddb13039b33347ed02d03737acf2c2a5d2
ろむねこ <nem@nem.x0.to>
2024-07-08 15:29:48 +0900

    実装 WIP

--- feature/child/src/main/java/one/nem/kidshift/feature/child/ChildManageMainActivity.java
+++ feature/child/src/main/java/one/nem/kidshift/feature/child/ChildManageMainActivity.java
@@ -1,14 +1,40 @@
 package one.nem.kidshift.feature.child;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+import one.nem.kidshift.data.ChildData;
+import one.nem.kidshift.feature.child.adapter.ChildListAdapter;
+import one.nem.kidshift.model.ChildModel;
+import one.nem.kidshift.model.callback.ChildModelCallback;
+import one.nem.kidshift.utils.KSLogger;
+import one.nem.kidshift.utils.factory.KSLoggerFactory;
+
+@AndroidEntryPoint
 public class ChildManageMainActivity extends AppCompatActivity {
+
+    @Inject
+    ChildData childData;
+
+    @Inject
+    KSLoggerFactory loggerFactory;
+
+    private KSLogger logger;
+
+    ChildListAdapter childListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +45,47 @@ public class ChildManageMainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        logger = loggerFactory.create("ChildManageMainActivity");
+
+        RecyclerView recyclerView = findViewById(R.id.childListRecyclerView);
+        childListAdapter = new ChildListAdapter();
+        childListAdapter.setButtonEventCallback(new ChildListAdapter.ButtonEventCallback() {
+            @Override
+            public void onEditButtonClick(ChildModel childModel) {
+                Toast.makeText(ChildManageMainActivity.this, "Edit button clicked", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLoginButtonClick(ChildModel childModel) {
+                Toast.makeText(ChildManageMainActivity.this, "Login button clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        recyclerView.setAdapter(childListAdapter);
+
+        updateList();
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private void updateList() {
+        childData.getChildList(new ChildModelCallback() {
+            @Override
+            public void onUnchanged() {
+
+            }
+
+            @Override
+            public void onUpdated(List<ChildModel> childModelList) {
+
+            }
+
+            @Override
+            public void onFailed(String message) {
+
+            }
+        }).thenAccept(childListAdapter::setChildList).thenRun(() -> {
+            runOnUiThread(() -> childListAdapter.notifyDataSetChanged());
         });
     }
 }
