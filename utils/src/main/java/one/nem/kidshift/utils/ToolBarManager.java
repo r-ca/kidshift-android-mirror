0993e46d4870bcee93ac7c1a710b3f2308bc2b7d
ろむねこ <nem@nem.x0.to>
2024-07-09 11:45:32 +0900

    ToolbarManager実装 WIP

---
+++ utils/src/main/java/one/nem/kidshift/utils/ToolBarManager.java
@@ -0,0 +1,60 @@
+package one.nem.kidshift.utils;
+
+import androidx.appcompat.widget.Toolbar;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class ToolBarManager {
+
+    private Toolbar toolbar;
+
+    @Inject
+    public ToolBarManager() {
+    }
+
+    public void setToolbar(Toolbar toolbar) {
+        this.toolbar = toolbar;
+    }
+
+    private void checkToolbar() {
+        if (toolbar == null) {
+            throw new IllegalStateException("Toolbar is not set");
+        }
+    }
+
+    public void setTitle(String title) {
+        checkToolbar();
+        toolbar.setTitle(title);
+    }
+
+    public void setSubtitle(String subtitle) {
+        checkToolbar();
+        toolbar.setSubtitle(subtitle);
+    }
+
+    public void setNavigationIcon(int resId) {
+        checkToolbar();
+        toolbar.setNavigationIcon(resId);
+    }
+
+    public void setMenuResId(int resId) {
+        checkToolbar();
+        toolbar.inflateMenu(resId);
+    }
+
+    public void setOnMenuItemClickListener(Toolbar.OnMenuItemClickListener listener) {
+        checkToolbar();
+        toolbar.setOnMenuItemClickListener(listener);
+    }
+
+    public void setNavigationOnClickListener(Toolbar.OnClickListener listener) {
+        checkToolbar();
+        toolbar.setNavigationOnClickListener(listener);
+    }
+
+    public Toolbar getToolbar() {
+        return toolbar;
+    }
+}
