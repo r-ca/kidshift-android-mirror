c846659e4b37602fecfda91b4eb6a46d2587f6c1
ろむねこ <nem@nem.x0.to>
2024-07-09 16:00:43 +0900

    Singleton化

--- data/src/main/java/one/nem/kidshift/data/modules/ParentDataModule.java
+++ data/src/main/java/one/nem/kidshift/data/modules/ParentDataModule.java
@@ -4,11 +4,12 @@ import dagger.Binds;
 import dagger.Module;
 import dagger.hilt.InstallIn;
 import dagger.hilt.android.components.FragmentComponent;
+import dagger.hilt.components.SingletonComponent;
 import one.nem.kidshift.data.ParentData;
 import one.nem.kidshift.data.impl.ParentDataImpl;
 
 @Module
-@InstallIn(FragmentComponent.class)
+@InstallIn(SingletonComponent.class)
 public abstract class ParentDataModule {
 
     @Binds
