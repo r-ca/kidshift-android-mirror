49e8acbe2f0a127a1dd19f9a6d70b47f8efd5676
ろむねこ <nem@nem.x0.to>
2024-07-22 11:40:27 +0900

    Adapterひな形実装

---
+++ feature/wallet/src/main/java/one/nem/kidshift/wallet/HistoryItemListAdapter.java
@@ -0,0 +1,34 @@
+package one.nem.kidshift.wallet;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HistoryItemListAdapter extends RecyclerView.Adapter<HistoryItemListAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public HistoryItemListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HistoryItemListAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView historyItemNameTextView;
+        TextView historyItemRewardTextView;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
