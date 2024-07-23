7ea5e143d64709a0bd9ee80acf1700c91c0c7ef5
ろむねこ <nem@nem.x0.to>
2024-07-23 11:07:01 +0900

    暫定ヘッダーを挿入するように

--- feature/wallet/src/main/java/one/nem/kidshift/wallet/HistoryItemListAdapter.java
+++ feature/wallet/src/main/java/one/nem/kidshift/wallet/HistoryItemListAdapter.java
@@ -127,6 +127,7 @@ public class HistoryItemListAdapter extends RecyclerView.Adapter<HistoryItemList
             LinearLayout view = new LinearLayout(parent.getContext());
             // 横全幅
             view.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            view.addView(LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_history_month_header, parent, false));
             view.addView(LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_history_list_item, parent, false));
             return new ViewHolder(view);
         } else {
